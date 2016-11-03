3c3a1c68afc5eb88bb9b27a2cdb7395faa62429d
Cameron Poe <cameron@zay.li>
2016-11-03 16:49:57 -0500

    Update udpflood.go

--- udpflood.go
+++ udpflood.go
@@ -6,14 +6,14 @@ import (
     "fmt"
 )
 
-func main () {
-    //Parse command line flags
-    var (
+var (
         _host     = kingpin.Flag("host", "Hostname").Short('h').Required().String()
         _port     = kingpin.Flag("port", "Port number").Short('p').Required().Int()
         _threads     = kingpin.Flag("threads", "Thread count").Short('t').Default("1").Int()
         _size     = kingpin.Flag("size", "Packet Size").Short('s').Default("65507").Int()
     )
+
+func main () {
     kingpin.Parse()
 
     fullAddr := fmt.Sprintf("%s:%v", *_host, *_port)
